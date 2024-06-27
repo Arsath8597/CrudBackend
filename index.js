5b59d255bfa6c725b47f6e01a4a5eeea8a931138
Arsath8597 <arsath8597@gmail.com>
2024-06-27 15:59:16 +0530

    new Changes

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ import cors from 'cors'
 const app=express();
 const PORT=2717;
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://crud-product-menagement-c9ozfqpxv-arsath8597s-projects.vercel.app'); // Update to match the domain you will make the request from
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
 // Allow requests from multiple origins
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://crud-product-menagement-c9ozfqpxv-arsath8597s-projects.vercel.app'
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+  'http://localhost:3000',
+  'https://crud-product-menagement-c9ozfqpxv-arsath8597s-projects.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
-  }));
+    return callback(null, true);
+  }
+}));
 app.use(bodyParser.json())
 
 app.use(express.json({ limit: "100mb" })); // Corrected middleware
